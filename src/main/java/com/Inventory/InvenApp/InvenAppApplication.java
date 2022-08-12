ad42d607ae28d1d45c1aac3e807f87d46d1c02ab
Sohail Adhikari <sohailadhikari@gmail.com>
2022-08-11 19:14:02 -0700

    new comit

---
+++ src/main/java/com/Inventory/InvenApp/InvenAppApplication.java
@@ -0,0 +1,13 @@
+package com.Inventory.InvenApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvenAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvenAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Inventory/InvenApp/InvenAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Inventory.InvenApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvenAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
